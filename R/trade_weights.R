eba67435eaca15304722c9b2acadd76da81aad69
Jason Muhlenkamp <jason.muhlenkamp@gmail.com>
2018-10-22 14:55:34 -0300

    Add example to trade_weights()

--- R/trade_weights.R
+++ R/trade_weights.R
@@ -6,6 +6,26 @@
 #' @param df_prices a data.frame containing the columns: symbol, date, price
 #' @param df_weights a data.frame containing the columns: symbol, date, w
 #' @export
+#' @examples
+#' library(quantmod)
+#'
+#' # Create a data.frame of adjusted close prices
+#' getSymbolsData <- new.env()
+#' getSymbols(c('XLB', 'XLE', 'XLF'),
+#'            env = getSymbolsData,
+#'            from = as.Date("2018-09-03"),
+#'            to = as.Date("2018-09-29"))
+#' df_close <- dplyr::bind_rows(
+#'     lapply(getSymbolsData, function(x)
+#'         data.frame(date = index(x), price = unname(Ad(x)))), .id = 'symbol')
+#'
+#' # Create a data.frame of weights
+#' df_weights <- df_close
+#' df_weights$w <- 0.32
+#' df_weights$price <- NULL
+#'
+#' # Trade the weights
+#' df_nav <- trade_weights(df_close, df_weights)
 
 trade_weights <- function(df_prices, df_weights) {
     validate_df_columns(df_prices, c("symbol", "date", "price"))
