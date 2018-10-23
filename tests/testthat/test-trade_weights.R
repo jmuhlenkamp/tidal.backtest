6e163b36d32b05c936f2095b5d556df7a6d1da2d
Jason Muhlenkamp <jason.muhlenkamp@gmail.com>
2018-10-23 11:20:26 -0300

    Add unit test of final nav value from trade_weights()

--- tests/testthat/test-trade_weights.R
+++ tests/testthat/test-trade_weights.R
@@ -3,3 +3,16 @@ context("Test trade_weights()")
 test_that("trade_weights() handles errors correctly", {
     expect_error(trade_weights(1:26, LETTERS))
 })
+
+test_that("trade_weights() final nav within 1 bp excel calculation", {
+    expect_equal(
+        dplyr::pull(
+            dplyr::filter(
+                trade_weights(etfdailyclose, dplyr::mutate(etfdailyclose, w = 1/3)),
+                dplyr::row_number() == dplyr::n()),
+            nav
+            ) / 977866.109536611 - 1,
+        0,
+        tolerance = 0.0001,
+        scale = 1)
+})
