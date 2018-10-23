2cdf528e146795ceb9c3f7110cff1ad47d49cbfa
Jason Muhlenkamp <jason.muhlenkamp@gmail.com>
2018-10-23 17:50:35 -0300

    Add convert_fedfunds_broker function and a unit test for it along with minor simplification to documentation of other convert_* functions

--- R/fedfunds_functions.R
+++ R/fedfunds_functions.R
@@ -1,6 +1,6 @@
-#' Unexported function(s) used to munge fedfunds and related data
+#' Unexported function(s) to munge fedfunds and related data
 #'
-#' Functions used to change fedfunds style data into price style data.
+#' Changes fedfunds style data into price style data.
 #'
 #' @param df data.frame containing columns: symbol, date, rate (annualized, e.g. 2.25).
 #'
@@ -13,9 +13,9 @@ convert_rate_g <- function(df) {
     as.data.frame(as.data.table(df)[,g:=c(0, (1+rate[-.N]/100)^(1/365)-1)])
 }
 #'
-#' Unexported function(s) used to munge fedfunds and related data
+#' Unexported function(s) to munge fedfunds and related data
 #'
-#' Functions used to change fedfunds growth data into price style data.
+#' Changes fedfunds growth data into price style data.
 #'
 #' @param df data.frame containing columns: symbol, date, g (daily growth, e.g. 0.01 = 1 percent).
 #'
@@ -26,4 +26,18 @@ convert_g_price <- function(df) {
              "Input df can only contain one unique symbol.")
     }
     as.data.frame(as.data.table(df)[,price:=cumprod(1+g)])
+}
+#'
+#' Unexported function(s) to munge fedfunds and related data
+#'
+#' Converts fedfunds symbol and rates into broker style symbols and rates
+#'
+#' @param df data.frame containing columns: symbol, date, rate (annualized, e.g. 2.25).
+#' @param new_symbol character string to use as symbol column on returned data.frame.
+#' @param add_rate Annualized rate to add to input rate (e.g. 0.25 = +25 basis points).
+#'
+convert_fedfunds_broker <- function(df, new_symbol, add_rate) {
+    as.data.frame(as.data.table(df)[,`:=`(symbol=new_symbol,
+                                          rate=ifelse(rate+add_rate < 0, 0,
+                                                      rate+add_rate))])
 }
--- tests/testthat/test-fedfunds_functions.R
+++ tests/testthat/test-fedfunds_functions.R
@@ -1,5 +1,10 @@
 context("Test fedfunds Functions")
 
+test_that("convert_fedfunds_broker() does not return negative rate", {
+    expect_identical(min(convert_fedfunds_broker(fedfunds, "_CASH_LONG_", -9.00)$rate) >= 0,
+                     TRUE)
+})
+
 test_that("convert_*() final price within 1 bp excel calculation", {
     expect_equal(tail(convert_g_price(convert_rate_g(fedfunds)), 1)$price /
                      1.01712548616142 - 1,
