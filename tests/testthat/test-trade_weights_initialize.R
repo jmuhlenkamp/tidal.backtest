228a381c65566b1a3c72e786daf50b83010423bf
Jason Muhlenkamp <jason.muhlenkamp@gmail.com>
2018-10-23 10:58:52 -0300

    Add unit tests for trade_weights_initialize()

---
+++ tests/testthat/test-trade_weights_initialize.R
@@ -0,0 +1,12 @@
+context("Test trade_weights_initialize()")
+
+test_that("trade_weights_initialize() handles errors correctly", {
+    expect_error(trade_weights_initialize(1:26, LETTERS))
+})
+
+test_that("trade_weights_initialize() return list does not contain NAs", {
+    list_initialized <- trade_weights_initialize(etfdailyclose,
+                                                 dplyr::mutate(etfdailyclose, w = 1/3))
+    list_nas <- lapply(list_initialized, function(x) any(is.na(x)))
+    expect_identical(list_nas, list(dates = FALSE, dt = FALSE, dt_fund = FALSE))
+})
