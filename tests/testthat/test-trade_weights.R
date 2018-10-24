69d561f144b63f6b58bbf670c11991f4394020a1
Jason Muhlenkamp <jason.muhlenkamp@gmail.com>
2018-10-24 11:07:42 -0300

    Re-organize code within trade-trade_weights.R to make it easier to add weekly, monthly, and quarterly rebal units tests

--- tests/testthat/test-trade_weights.R
+++ tests/testthat/test-trade_weights.R
@@ -1,51 +1,30 @@
-context("Test trade_weights()")
-etfs_5days <- etfdailyclose[etfdailyclose$date <= as.Date('2017-10-30'),]
-etfs_5days_cash <- dplyr::bind_rows(create_cash_assets(fedfunds), etfs_5days)
-
-readr::write_csv(etfs_5days_cash, '~/Desktop/df.csv')
-
+context("trade_weights() basics")
 test_that("trade_weights() handles errors correctly", {
     expect_error(trade_weights(1:26, LETTERS))
 })
 
-test_that(paste("trade_weights() final nav within 1 bp excel calculation:",
-                "100% Long Exposure & daily rebalance"), {
-    expect_equal(
-        dplyr::pull(
-            dplyr::filter(
-                trade_weights(etfs_5days, dplyr::mutate(etfdailyclose, w = 1/3)),
-                dplyr::row_number() == dplyr::n()),
-            nav
-            ) / 1003182.5310446 - 1,
-        0,
-        tolerance = 0.0001,
-        scale = 1)
-})
+context("trade_weights() final nav within 1 bp excel calculation")
 
-test_that(paste("trade_weights() final nav within 1 bp excel calculation:",
-                "50% Long Exposure & daily rebalance"), {
-        expect_equal(
-            dplyr::pull(
-                dplyr::filter(
-                    trade_weights(etfs_5days_cash, dplyr::mutate(etfdailyclose, w = 1/6)),
-                    dplyr::row_number() == dplyr::n()),
-                nav
-            ) / 1001675.91084044 - 1,
-            0,
-            tolerance = 0.0001,
-            scale = 1)
-})
+etfs_5days <- etfdailyclose[etfdailyclose$date <= as.Date('2017-10-30'),]
+etfs_5days_cash <- dplyr::bind_rows(create_cash_assets(fedfunds), etfs_5days)
 
-test_that(paste("trade_weights() final nav within 1 bp excel calculation:",
-                "150% Long Exposure & daily rebalance"), {
-    expect_equal(
-        dplyr::pull(
-            dplyr::filter(
-                trade_weights(etfs_5days_cash, dplyr::mutate(etfdailyclose, w = 1/2)),
-                dplyr::row_number() == dplyr::n()),
-            nav
-        ) / 1004631.17245305 - 1,
-        0,
-        tolerance = 0.0001,
-        scale = 1)
-})
+daily_long100 <- dplyr::pull(dplyr::filter(
+    trade_weights(etfs_5days, dplyr::mutate(etfdailyclose, w = 1/3)),
+    dplyr::row_number() == dplyr::n()),
+    nav)
+test_that("Final Nav: Daily Rebalance & 100% Long Exposure",
+          expect_equal(daily_long100 / 1003182.5310446 - 1,
+                        0, tolerance = 0.0001, scale = 1))
+
+daily_long050 <- dplyr::pull(dplyr::filter(
+    trade_weights(etfs_5days_cash, dplyr::mutate(etfdailyclose, w = 1/6)),
+    dplyr::row_number() == dplyr::n()),
+    nav)
+test_that("Final Nav: Daily Rebalance & 50% Long Exposure",
+          expect_equal(daily_long050 / 1001675.91084044 - 1,
+                       0, tolerance = 0.0001, scale = 1))
+
+daily_long150 <- dplyr::pull(dplyr::filter(
+    trade_weights(etfs_5days_cash, dplyr::mutate(etfdailyclose, w = 1/2)),
+    dplyr::row_number() == dplyr::n()),
+    nav)
