76b0fef5f724b6cde4f53312e34d2da91f2ec928
Jason Muhlenkamp <jason.muhlenkamp@gmail.com>
2018-10-24 10:44:39 -0300

    Add rebal_ith and rebal_group_by parameters to trade_weights()

--- R/trade_weights.R
+++ R/trade_weights.R
@@ -5,6 +5,14 @@
 #'
 #' @param df_prices a data.frame containing the columns: symbol, date, price
 #' @param df_weights a data.frame containing the columns: symbol, date, w
+#' @param rebal_ith Optional Integer to determine which observation of each by group to return.
+#'                 For Example ith_date = 2, week will return the second day of
+#'                 each week.  Also, ith_date = 99, month will return last day of
+#'                 each month.  Default is 1.
+#' @param rebal_group_by Optional Character indicating by group to apply to ith_date.
+#'                       Acceptable values are: date, week, month, qtr.  Default is date
+#'                       which performs a daily rebalance.
+#'
 #' @import data.table
 #' @export
 #' @examples
@@ -30,14 +38,14 @@
 #' df_nav <- trade_weights(df_close, df_weights)
 #' }
 
-trade_weights <- function(df_prices, df_weights) {
+trade_weights <- function(df_prices, df_weights, rebal_ith = 1, rebal_group_by = "date") {
     has_cash_long <- as.logical(nrow(df_prices[df_prices$symbol == "_CASH_LONG_",]))
     has_cash_short <- as.logical(nrow(df_prices[df_prices$symbol == "_CASH_SHORT_",]))
 
     list_initialized <- trade_weights_initialize(
         df_prices, df_weights, has_cash_long, has_cash_short)
     list_initialized$rebaldates <-
-        trade_weights_initialize_rebaldates(list_initialized$dates, 1, 'date')
+        trade_weights_initialize_rebaldates(list_initialized$dates, rebal_ith, rebal_group_by)
 
     list_executed <- trade_weights_execute(list_initialized)
 
